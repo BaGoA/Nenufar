bbcfdb6e67c6bd0bda17613acd14e43efd660084
Bastian Gonzalez Acevedo <bastiangonzalezacevedo@gmail.com>
2023-07-11 19:48:11 +0200

    Add activation functions in neural net

--- src/neural_net.rs
+++ src/neural_net.rs
@@ -3,32 +3,38 @@ use super::blaf;
 use super::num_gen::NumberGenerator;
 use super::topology::Topology;
 
-/// Neural network is representating by vector of weight matrix and vector of bias vector.
+/// Neural network is representating by vector of weight matrix, vector of bias vector
+/// and vector of activation function
 pub struct NeuralNet {
-    weigth: Vec<blaf::Matrix>,
+    weigths: Vec<blaf::Matrix>,
     bias: Vec<blaf::ColumnVector>,
+    activation_functions: Vec<Box<dyn ActivationFunction>>,
 }
 
 impl NeuralNet {
     /// Construct a neural network from topology
-    pub fn new<Generator>(topology: &Topology, random_gen: &Generator) -> Self
+    pub fn new<Generator>(topology: Topology, random_gen: &Generator) -> Self
     where
         Generator: NumberGenerator,
     {
         let nb_layer: usize = topology.nb_neurons.len();
 
-        let mut weigth: Vec<blaf::Matrix> = Vec::with_capacity(nb_layer);
+        let mut weigths: Vec<blaf::Matrix> = Vec::with_capacity(nb_layer);
         let mut bias: Vec<blaf::ColumnVector> = Vec::with_capacity(nb_layer);
 
         for id in 0..(nb_layer - 1) {
             let nb_rows: usize = topology.nb_neurons[id + 1];
             let nb_cols: usize = topology.nb_neurons[id];
 
-            weigth.push(blaf::Matrix::new(nb_rows, nb_cols, random_gen));
+            weigths.push(blaf::Matrix::new(nb_rows, nb_cols, random_gen));
             bias.push(random_gen.generate_vec(nb_rows));
         }
 
-        return Self { weigth, bias };
+        return Self {
+            weigths,
+            bias,
+            activation_functions: topology.activation_functions,
+        };
     }
 }
 
@@ -60,26 +66,29 @@ mod tests {
 
     #[test]
     fn test_neural_net_new() {
+        let nb_neurons: Vec<usize> = vec![2, 3, 1];
+
         let topology: Topology = TopologyBuilder::new()
-            .nb_input(2)
-            .add_layer(3, Box::new(TestActivationFn::default()))
-            .add_layer(1, Box::new(TestActivationFn::default()))
+            .nb_input(nb_neurons[0])
+            .add_layer(nb_neurons[1], Box::new(TestActivationFn::default()))
+            .add_layer(nb_neurons[2], Box::new(TestActivationFn::default()))
             .build()
             .unwrap();
 
         let generator: ZeroGenerator = ZeroGenerator::default();
 
-        let neural_net: NeuralNet = NeuralNet::new(&topology, &generator);
+        let neural_net: NeuralNet = NeuralNet::new(topology, &generator);
 
-        let size: usize = topology.nb_neurons.len() - 1;
+        let size: usize = nb_neurons.len() - 1;
 
-        assert_eq!(neural_net.weigth.len(), size);
+        assert_eq!(neural_net.weigths.len(), size);
         assert_eq!(neural_net.bias.len(), size);
+        assert_eq!(neural_net.activation_functions.len(), size);
 
         for id in 0..size {
-            assert_eq!(neural_net.weigth[id].nb_rows(), topology.nb_neurons[id + 1]);
-            assert_eq!(neural_net.weigth[id].nb_columns(), topology.nb_neurons[id]);
-            assert_eq!(neural_net.bias[id].len(), topology.nb_neurons[id + 1]);
+            assert_eq!(neural_net.weigths[id].nb_rows(), nb_neurons[id + 1]);
+            assert_eq!(neural_net.weigths[id].nb_columns(), nb_neurons[id]);
+            assert_eq!(neural_net.bias[id].len(), nb_neurons[id + 1]);
         }
     }
 }
